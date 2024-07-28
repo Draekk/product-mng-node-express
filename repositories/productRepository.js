fa07586b4ff4a4de1cff15d5f5557c4d121bfe23
draekkdev <rodriver1992@gmail.com>
2024-07-28 17:41:00 -0400

    added documentation on productRepository.js

--- repositories/productRepository.js
+++ repositories/productRepository.js
@@ -18,6 +18,13 @@ function findAll() {
   }
 }
 
+/**
+ * Retrieves a product from the database by its ID.
+ *
+ * @param {number} id - The unique identifier of the product to retrieve.
+ * @returns {Product|null} A Product object if found, or null if not found or an error occurs.
+ * @throws Will throw an error if there is a problem with the database connection or data retrieval.
+ */
 function findById(id) {
   try {
     let product = jsonProducts.products.find((p) => {
