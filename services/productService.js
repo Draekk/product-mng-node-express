39df7b599dc08723202aefe2970d594b841ba5f7
draekkdev <rodriver1992@gmail.com>
2024-07-30 19:32:10 -0400

    updated function on services

--- services/productService.js
+++ services/productService.js
@@ -1,51 +1,103 @@
 const repository = require("../repositories/productRepository");
 const Product = require("../entities/product");
+const path = require("path");
 
+/**
+ * Retrieves all products from the repository.
+ *
+ * @returns {Product[]|null} An array of Product objects or null if an error occurs.
+ */
 function findProducts() {
   return repository.findAll();
 }
 
+/**
+ * Retrieves a product by its ID from the repository.
+ *
+ * @param {number} id - The unique identifier of the product to retrieve.
+ * @returns {Product|null} The Product object with the specified ID, or null if not found or an error occurs.
+ */
 function findProductById(id) {
   return repository.findById(id);
 }
 
+/**
+ * Retrieves a list of products whose names match the provided name exactly (case-insensitive).
+ *
+ * @param {string} name - The name of the products to retrieve.
+ * @returns {Product[]|null} An array of Product objects that match the provided name, or null if an error occurs.
+ *
+ * @throws Will throw an error if the provided name is not a string.
+ */
 function findProductsByName(name) {
+  if (typeof name !== "string") {
+    throw new Error("Nombre inválido");
+  }
   try {
     return repository
       .findAll()
       .filter((p) => p._name.toLowerCase() === name.trim().toLowerCase());
   } catch (error) {
-    console.error(`Ha ocurrido un error: ${error}`);
+    console.error(error);
     return null;
   }
 }
 
+/**
+ * Retrieves a list of products whose names contain the provided text (case-insensitive).
+ *
+ * @param {string} text - The text to search for in the product names.
+ * @returns {Product[]|null} An array of Product objects that contain the provided text in their names, or null if an error occurs.
+ *
+ * @throws Will throw an error if the provided text is not a string.
+ */
 function findProductsByNameContaining(text) {
   try {
+    if (typeof text !== "string") {
+      throw new Error("Texto inválido");
+    }
     return repository
       .findAll()
       .filter((p) => p._name.toLowerCase().match(text.trim().toLowerCase()));
   } catch (error) {
-    console.error(`Ha ocurrido un error: ${error}`);
+    console.error(error);
     return null;
   }
 }
 
-function save(productJson) {
+function saveProduct({ name, price }) {
   try {
+    if (typeof name !== "string" || typeof price !== "number") {
+      throw new Error("Tipos de datos inválidos");
+    }
     let product = new Product();
-    product._name = productJson.name;
-    product._price = productJson.price;
+    product._name = name;
+    product._price = price;
 
     return repository.save(product);
   } catch (error) {
-    console.error(`Ha ocurrido un error: ${error}`);
+    console.error(`${error}`);
     return null;
   }
 }
 
-function update(productJson) {
+/**
+ * Updates an existing product in the repository.
+ *
+ * @param {Object} productJson - The JSON object representing the product to update.
+ * @param {number} productJson.id - The unique identifier of the product to update.
+ * @param {string} productJson.name - The new name of the product.
+ * @param {number} productJson.price - The new price of the product.
+ *
+ * @returns {Product|null} The updated Product object, or null if an error occurs.
+ *
+ * @throws Will throw an error if the provided productJson is not an object, if it does not contain the required properties, or if the ID is invalid.
+ */
+function updateProduct({ id, name, price }) {
   try {
+    if ((typeof id, price !== "number" || typeof name !== "string")) {
+      throw new Error("Tipos de datos inválidos.");
+    }
     let product = new Product();
     product._id = productJson.id;
     product._name = productJson.name;
@@ -53,9 +105,26 @@ function update(productJson) {
 
     return repository.save(product);
   } catch (error) {
-    console.error(`Ha ocurrido un error: ${error}`);
+    console.error(`${error}`);
     return null;
   }
 }
 
-console.log(update({ id: 3, name: "Lemons", price: 80 }));
+function deleteProduct(id) {
+  if (typeof id !== "number" || isNaN(id) || id <= 0) {
+    throw new Error("ID inválido");
+  }
+  repository.destroyById(id);
+}
+
+console.log(updateProduct({ id: "1", name: "something", price: 123 }));
+
+module.exports = {
+  findProducts,
+  findProductById,
+  findProductsByName,
+  findProductsByNameContaining,
+  saveProduct,
+  updateProduct,
+  deleteProduct,
+};
