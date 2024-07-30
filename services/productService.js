bef13c2e3047d8dd0c2f731087b6a5b25bbbac3a
draekkdev <rodriver1992@gmail.com>
2024-07-30 12:06:46 -0400

    added search containing function to service

--- services/productService.js
+++ services/productService.js
@@ -15,7 +15,19 @@ function findProductsByName(name) {
       .filter((p) => p._name.toLowerCase() === name.trim().toLowerCase());
   } catch (error) {
     console.error(`Ha ocurrido un error: ${error}`);
+    return null;
   }
 }
 
-console.log(findProductsByName("bREAD "));
+function findProductsByNameContaining(text) {
+  try {
+    return repository
+      .findAll()
+      .filter((p) => p._name.toLowerCase().match(text.trim().toLowerCase()));
+  } catch (error) {
+    console.error(`Ha ocurrido un error: ${error}`);
+    return null;
+  }
+}
+
+console.log(findProductsByNameContaining("es "));
